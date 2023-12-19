4daac607075559814fff4daf121de6c18cbb4fe3
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-12-19 11:26:32 +0000

    initial commit

---
+++ 2023/day06/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	// "util"
+)
+
+var input string
+
+func main() {
+	fmt.Println("Day 06")
+
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+	fmt.Println("Running part", part)
+
+	if part == 1 {
+		ans := part1(input)
+		// util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(input)
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input string) int {
+	return 0
+}
+func part2(input string) int {
+	return 0
+}
