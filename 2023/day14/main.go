bb8af8c2158d700c7ad9522edfdfce09d72d137c
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-12-21 11:20:31 +0000

    stubbed out day 14

---
+++ 2023/day14/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"fmt"
+	"strings"
+
+	"advent-of-code-go/util"
+)
+
+//go:embed input.txt
+var input string
+
+//go:embed example1.txt
+var example1 string
+
+func init() {
+	// do this in init (not main) so test file has same input
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+
+	example1 = strings.TrimRight(example1, "\n")
+	if len(input) == 0 {
+		panic("empty example1.txt file")
+	}
+}
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	var inputType string
+	flag.StringVar(&inputType, "inputType", "puzzle", "puzzle or example1")
+	flag.Parse()
+
+	var tempInput string
+	if inputType == "puzzle" {
+		tempInput = input
+	} else {
+		tempInput = example1
+	}
+	if part == 1 {
+		ans := part1(tempInput)
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(tempInput)
+		util.CopyToClipboard(fmt.Sprintf("%v", ans))
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input string) int {
+	parsed := parseInput(input)
+	fmt.Println(parsed[0])
+	_ = parsed
+
+	return 0
+}
+
+func part2(input string) int {
+	return 0
+}
+
+func parseInput(input string) (ans [][]string) {
+	for _, line := range strings.Split(input, "\n") {
+		ans = append(ans, strings.Split(line, ""))
+	}
+	return ans
+}
